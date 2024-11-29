918c8abb058f91ac7bf768c4fdf37b765c69af34
Bozomil Milanovic <bozomil@gmail.com>
2024-11-29 13:18:25 +0100

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,19 @@
-import { fileURLToPath, URL } from 'node:url';
-
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { fileURLToPath, URL } from 'node:url';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   server: {
     port: 3011,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'http://localhost:5000', // json-server u istom kontejneru
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
+    host: '0.0.0.0', // Osigurajte da je aplikacija dostupna izvan kontejnera
   },
   resolve: {
     alias: {
